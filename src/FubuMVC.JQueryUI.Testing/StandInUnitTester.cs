837cb25d2f3c1c914f5b4d12c0c50969f5b9f646
Jeremy Miller <jeremydmiller@yahoo.com>
2012-08-02 10:00:10 -0500

    initial import of code

---
+++ src/FubuMVC.JQueryUI.Testing/StandInUnitTester.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace FubuMVC.JQueryUI.Testing
+{
+    [TestFixture]
+    public class StandInUnitTester
+    {
+        [Test]
+        public void all_ok()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
